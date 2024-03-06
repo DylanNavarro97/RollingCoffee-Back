3c20d0627025cc1e42ef8f36ab895836d574e387
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-06 10:28:43 -0300

    agregamos una ruta en productos.routes y su respectivo controlador

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import "dotenv/config"; //permite procesar variables de entorno
 import morgan from "morgan";
 import { fileURLToPath } from "url";
 import path from "path";
+import productosRouter from "./src/routes/productos.routes.js";
 // 1- configurar un puerto
 // puedo compilar el index.js usando node --watch index.js en forma experimental.
 const app = express();
@@ -24,8 +25,9 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '/public')))
 
 //3 - Configurar las rutas
-app.get("/nuevo", (req, res) => {
-  console.log("alguien solicito algo");
-  // falta configurar la respuesta
-  res.send("respuesta desde nuestro backend de rollingCoffee");
-});
+app.use('/api', productosRouter)
+// app.get("/nuevo", (req, res) => {
+//   console.log("alguien solicito algo");
+//   // falta configurar la respuesta
+//   res.send("respuesta desde nuestro backend de rollingCoffee");
+// });
