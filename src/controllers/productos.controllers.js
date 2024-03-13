d6aff99e11e197d48c926c5a5777e62b67c22421
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-13 11:24:28 -0300

    agregamos la funcion para editar un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -44,3 +44,18 @@ export const crearProducto = async (req, res) => {
     });
   }
 };
+
+export const editarProducto = async (req, res) => {
+  try {
+    const buscarProducto = await Producto.findById(req.params.id)
+    if (!buscarProducto){
+      return res.status(404).json({mensaje: 'No se pudo editar el producto, el id es incorrecto'})
+    }
+
+    await Producto.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({mensaje: 'El producto fue modificado exitosamente'})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({mensaje: 'Ocurrió un error al intentar editar un producto'})
+  }
+} 
